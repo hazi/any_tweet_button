92ceb26ad2c820540755c1a566d02486c3cba1f6
yuhei mukoyama <yuhei.mukoyama@gmail.com>
2019-02-02 19:51:23 +0900

    :sparkles: Amazon URL optimization and bugfix
    * add Amazon URL optimization
    * Corresponds to URLs starting with //

--- inject.js
+++ inject.js
@@ -1,10 +1,40 @@
-function openTweetWindow(url){
+function openTweetWindow(url) {
   n = window.screen.height,
   r = window.screen.width,
   i = Math.round(r / 2 - 275),
   o = 0,
   n > 420 && (o = Math.round(n / 2 - 210)),
   window.open(url, null, "scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,left=" + i + ",top=" + o)
+}
+
+function urlOptimize(url){
+  let resultURL = url;
+
+  if(url.match(/^\/\//)) { resultURL = document.location.protocol + resultURL }
+
+  amazon = resultURL.match(/^(https?:\/\/)(www.)?(amazon.co.jp\/)(?:.+?\/)(dp\/.+$)/);
+  if(amazon) {
+    let arr = amazon.filter(Boolean)
+    arr.splice(0,1)
+    resultURL = arr.join('')
+  }
+
+  return resultURL
+}
+
+function getURL() {
+  let ogURL = document.querySelector('meta[property="og:url"]');
+  ogURL = ogURL && ogURL.content;
+
+  let linkURL = document.querySelector('link[rel="canonical"]');
+  linkURL = linkURL && linkURL.href;
+
+  return [ogURL, linkURL, location.href].filter(Boolean)[0]
+}
+
+function getTitle() {
+  const ogTitle = document.querySelector('meta[property="og:title"]');
+  return ogTitle && ogTitle.content || document.title;
 }
 
 {
@@ -14,11 +44,6 @@ function openTweetWindow(url){
     'a[href^="http://twitter.com/share"]'
   ]
   const node = document.querySelector(nodeQuery.join(','));
-  const ogTitle = document.querySelector('meta[property="og:title"]');
-  const ogUrl = document.querySelector('meta[property="og:url"]');
-
-  const url = ogUrl && ogUrl.content || location.href;
-  const title = ogTitle && ogTitle.content || document.title;
 
   let tweet;
   if (node && node.nodeName === 'IFRAME') {
@@ -27,6 +52,8 @@ function openTweetWindow(url){
   } else if (node) {
     tweet = node.href;
   } else {
+    const url = urlOptimize(getURL());
+    const title = getTitle();
     tweet = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodeURIComponent(url)
   };
 
